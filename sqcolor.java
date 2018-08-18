bffd9eb2c1c5ab0297c121069ea32ef63f58f52e
hemavasudevan <40655808+hemavasudevan@users.noreply.github.com>
2018-08-18 09:14:14 +0530

    Create sqcolor.java

---
+++ sqcolor.java
@@ -0,0 +1,13 @@
+package linecolor;
+import java.applet.Applet;  
+import java.awt.*; 
+
+		public class draw extends Applet{  
+		  
+		public void paint(Graphics g){  
+		g.setColor(Color.magenta);  
+		//g.drawLine(20,30,20,300);  
+		g.drawRect(70,100,30,30);  
+		g.fillRect(170,100,30,30); 
+		}  
+		}  
