cac7a54d56bb5d8a5725297a6bfa03bc6a6e1d68
hemavasudevan <40655808+hemavasudevan@users.noreply.github.com>
2018-08-18 09:10:52 +0530

    Create line.java

---
+++ line.java
@@ -0,0 +1,21 @@
+package line;
+import java.awt.*;
+
+	  	public class slpline extends Frame
+	{
+	  public slpline()
+	  {          // the following 3 lines creates an empty frame
+	    setTitle("Line Graphics by S N Rao");
+	    setSize(300, 350);
+	    setVisible(true);
+	  }
+	  public void paint(Graphics g)
+	  {        
+	    g.drawLine(60,90, 150, 200);
+	    g.drawLine(110, 120, 250, 270);
+	  }
+	  public static void main(String args[])
+	  {
+	    new slpline();
+	  }
+	}
